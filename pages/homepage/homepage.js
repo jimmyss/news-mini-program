9414ae07e2b75242e976f2eb75dc76acd67a5650
jimmyss <3240429693@qq.com>
2023-05-24 20:02:49 +0800

    主页基本完成

--- pages/homepage/homepage.js
+++ pages/homepage/homepage.js
@@ -5,7 +5,73 @@ Page({
    * 页面的初始数据
    */
   data: {
+    newsList:[
+      { 
+        title:'红星深度｜23只小熊猫之死：警方解救后寄养动物园 涉案野生动物救助待规范',
+        time:'2023-05-15',
+        picPath:'/pages/homepage/images/new1.png',
+        author:'成都商报红星新闻'
+      },
+      {
+        title:'曾荣立一等战功的战区上将司令员发声，透露最新信号！',
+        time:'2023-05-15',
+        picPath:'/pages/homepage/images/new2.png',
+        author:'政知见'
+      },
+      {
+        title:'山西10岁男孩失联身亡，嫌疑人系生母与继父 知情人：生母曾称是孩子偷钱挨打后跑了',
+        time:'2023-05-24',
+        picPath:'/pages/homepage/images/new3.png',
+        author:'成都商报红星新闻'
+      },
+      {
+        title:'红星观察｜珠峰攀登生意经：有尊享版套餐，还有“被人抬着上山”一条龙服务',
+        time:'2023-05-23',
+        picPath:'/pages/homepage/images/new4.png',
+        author:'红星深度'
+      },{
+        title:'武汉一小学生校园内被老师驾车碾死 校方：没酒驾，孩子跑到车前蹲着',
+        time:'2023-05-24',
+        picPath:'/pages/homepage/images/new5.png',
+        author:'都市快报橙柿互动'
+      },{
+        title:'今年会有多少新生儿？产科一线：过去一天分娩七八个，现在几天一个',
+        time:'2023-05-24',
+        picPath:'/pages/homepage/images/new6.png',
+        author:'每日经济新闻'
+      },{
+        title:'TikTok遭美国一州全面封杀！总裁周受资：违宪，已起诉',
+        time:'2023-05-24',
+        picPath:'/pages/homepage/images/new7.png',
+        author:'每日经济新闻'
+      },{
+        title:'四川一教师输液时不幸身亡，涉事诊所称尽全力抢救，卫健局介入调查',
+        time:'2023-05-24',
+        picPath:'/pages/homepage/images/new8.png',
+        author:'九派新闻'
+      },{
+        title:'福建厦门一男医生猥亵女体检者被拘留：卫健委决定吊销其执业资格',
+        time:'2023-05-24',
+        picPath:'/pages/homepage/images/new2.png',
+        author:'成都商报红星新闻'
+      },{
+        title:'山西10岁男孩失联身亡，嫌疑人系生母与继父 知情人：生母曾称是孩子偷钱挨打后跑了',
+        time:'2023-05-24',
+        picPath:'/pages/homepage/images/new2.png',
+        author:'成都商报红星新闻'
+      },{
+        title:'山西10岁男孩失联身亡，嫌疑人系生母与继父 知情人：生母曾称是孩子偷钱挨打后跑了',
+        time:'2023-05-24',
+        picPath:'/pages/homepage/images/new2.png',
+        author:'成都商报红星新闻'
+      },{
+        title:'山西10岁男孩失联身亡，嫌疑人系生母与继父 知情人：生母曾称是孩子偷钱挨打后跑了',
+        time:'2023-05-24',
+        picPath:'/pages/homepage/images/new2.png',
+        author:'成都商报红星新闻'
+      }
 
+    ]
   },
 
   /**
