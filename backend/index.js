4f0f5e07bec54c88bbc16220e104ff447b49bbd2
jimmyss <3240429693@qq.com>
2023-05-31 15:56:43 +0800

    完成主页的下拉刷新、初始化、触底加载功能

---
+++ backend/index.js
@@ -0,0 +1,535 @@
+let data={
+    newsList:[
+          { 
+            title:'红星深度｜23只小熊猫之死：警方解救后寄养动物园 涉案野生动物救助待规范',
+            time:'2023-05-15',
+            picPath:'/pages/homepage/images/new1.png',
+            author:'成都商报红星新闻'
+          },
+          {
+            title:'曾荣立一等战功的战区上将司令员发声，透露最新信号！',
+            time:'2023-05-15',
+            picPath:'/pages/homepage/images/new2.png',
+            author:'政知见'
+          },
+          {
+            title:'山西10岁男孩失联身亡，嫌疑人系生母与继父 知情人：生母曾称是孩子偷钱挨打后跑了',
+            time:'2023-05-24',
+            picPath:'/pages/homepage/images/new3.png',
+            author:'成都商报红星新闻'
+          },
+          {
+            title:'红星观察｜珠峰攀登生意经：有尊享版套餐，还有“被人抬着上山”一条龙服务',
+            time:'2023-05-23',
+            picPath:'/pages/homepage/images/new4.png',
+            author:'红星深度'
+          },{
+            title:'武汉一小学生校园内被老师驾车碾死 校方：没酒驾，孩子跑到车前蹲着',
+            time:'2023-05-24',
+            picPath:'/pages/homepage/images/new5.png',
+            author:'都市快报橙柿互动'
+          },{
+            title:'今年会有多少新生儿？产科一线：过去一天分娩七八个，现在几天一个',
+            time:'2023-05-24',
+            picPath:'/pages/homepage/images/new6.png',
+            author:'每日经济新闻'
+          },{
+            title:'TikTok遭美国一州全面封杀！总裁周受资：违宪，已起诉',
+            time:'2023-05-24',
+            picPath:'/pages/homepage/images/new7.png',
+            author:'每日经济新闻'
+          },{
+            title:'四川一教师输液时不幸身亡，涉事诊所称尽全力抢救，卫健局介入调查',
+            time:'2023-05-24',
+            picPath:'/pages/homepage/images/new8.png',
+            author:'九派新闻'
+          },{
+            title:'美国会众议长麦卡锡：我们向中国借钱，不是为了养美国这些闲人',
+            time:'2023-05-21',
+            picPath:'/pages/homepage/images/new9.png',
+            author:'观察者网'
+          },{ 
+            title:'塞尔维亚总统宣布27日辞去执政党主席职务，现场数十万人雨中集结',
+            time:'2023-05-27',
+            picPath:'/pages/homepage/images/new10.png',
+            author:'观察者网'
+          },///////////
+          {
+            title:'美国研究首次系统性定义“长新冠”，12种“后遗症”症状列入',
+            time:'2023-05-27',
+            picPath:'/pages/homepage/images/new11.png',
+            author:'第一财经'
+          },
+          {
+            title:'人均GDP吊打北上深，这些四线小城普遍“家里有矿、富得流油”',
+            time:'2023-05-27',
+            picPath:'/pages/homepage/images/new12.png',
+            author:'国民经略'
+          },
+          {
+            title:'9岁男孩失踪13年后尸骨现身山林地缝，两名村妇被警方带走',
+            time:'2023-05-27',
+            picPath:'/pages/homepage/images/new13.png',
+            author:'极目新闻'
+          },{
+            title:'男子称25元挂专家号，医生打20分钟电话聊拆迁：最后两句话打发我',
+            time:'2023-05-27',
+            picPath:'/pages/homepage/images/new14.png',
+            author:'极目新闻'
+          },{
+            title:'河北一派出所内烟花爆炸致4死2失联，4个月前当地曾发文禁止经营烟花',
+            time:'2023-05-27',
+            picPath:'/pages/homepage/images/new15.png',
+            author:'上游新闻'
+          },{
+            title:'25岁研究生受伤后舍不得买贵支架，致血栓转移到肺部，逝世后捐献器官',
+            time:'2023-05-27',
+            picPath:'/pages/homepage/images/new16.png',
+            author:'九派新闻'
+          },{
+            title:'艰难开往乌克兰的F-16战机',
+            time:'2023-05-27',
+            picPath:'/pages/homepage/images/new17.png',
+            author:'中国新闻周刊'
+          },{
+            title:'安徽女子死后被摘取器官案：被害人儿子涉嫌敲诈涉案医生被立案',
+            time:'2023-05-27',
+            picPath:'/pages/homepage/images/new18.png',
+            author:'澎湃新闻'
+          },
+          { 
+            title:'红星深度｜23只小熊猫之死：警方解救后寄养动物园 涉案野生动物救助待规范',
+            time:'2023-05-15',
+            picPath:'/pages/homepage/images/new1.png',
+            author:'成都商报红星新闻'
+          },
+          {
+            title:'曾荣立一等战功的战区上将司令员发声，透露最新信号！',
+            time:'2023-05-15',
+            picPath:'/pages/homepage/images/new2.png',
+            author:'政知见'
+          },////////////
+          {
+            title:'山西10岁男孩失联身亡，嫌疑人系生母与继父 知情人：生母曾称是孩子偷钱挨打后跑了',
+            time:'2023-05-24',
+            picPath:'/pages/homepage/images/new3.png',
+            author:'成都商报红星新闻'
+          },
+          {
+            title:'红星观察｜珠峰攀登生意经：有尊享版套餐，还有“被人抬着上山”一条龙服务',
+            time:'2023-05-23',
+            picPath:'/pages/homepage/images/new4.png',
+            author:'红星深度'
+          },{
+            title:'武汉一小学生校园内被老师驾车碾死 校方：没酒驾，孩子跑到车前蹲着',
+            time:'2023-05-24',
+            picPath:'/pages/homepage/images/new5.png',
+            author:'都市快报橙柿互动'
+          },{
+            title:'今年会有多少新生儿？产科一线：过去一天分娩七八个，现在几天一个',
+            time:'2023-05-24',
+            picPath:'/pages/homepage/images/new6.png',
+            author:'每日经济新闻'
+          },{
+            title:'TikTok遭美国一州全面封杀！总裁周受资：违宪，已起诉',
+            time:'2023-05-24',
+            picPath:'/pages/homepage/images/new7.png',
+            author:'每日经济新闻'
+          },{
+            title:'四川一教师输液时不幸身亡，涉事诊所称尽全力抢救，卫健局介入调查',
+            time:'2023-05-24',
+            picPath:'/pages/homepage/images/new8.png',
+            author:'九派新闻'
+          },{
+            title:'美国会众议长麦卡锡：我们向中国借钱，不是为了养美国这些闲人',
+            time:'2023-05-21',
+            picPath:'/pages/homepage/images/new9.png',
+            author:'观察者网'
+          },{ 
+            title:'塞尔维亚总统宣布27日辞去执政党主席职务，现场数十万人雨中集结',
+            time:'2023-05-27',
+            picPath:'/pages/homepage/images/new10.png',
+            author:'观察者网'
+          },
+          {
+            title:'美国研究首次系统性定义“长新冠”，12种“后遗症”症状列入',
+            time:'2023-05-27',
+            picPath:'/pages/homepage/images/new11.png',
+            author:'第一财经'
+          },
+          {
+            title:'人均GDP吊打北上深，这些四线小城普遍“家里有矿、富得流油”',
+            time:'2023-05-27',
+            picPath:'/pages/homepage/images/new12.png',
+            author:'国民经略'
+          },//////////////
+          {
+            title:'9岁男孩失踪13年后尸骨现身山林地缝，两名村妇被警方带走',
+            time:'2023-05-27',
+            picPath:'/pages/homepage/images/new13.png',
+            author:'极目新闻'
+          },{
+            title:'男子称25元挂专家号，医生打20分钟电话聊拆迁：最后两句话打发我',
+            time:'2023-05-27',
+            picPath:'/pages/homepage/images/new14.png',
+            author:'极目新闻'
+          },{
+            title:'河北一派出所内烟花爆炸致4死2失联，4个月前当地曾发文禁止经营烟花',
+            time:'2023-05-27',
+            picPath:'/pages/homepage/images/new15.png',
+            author:'上游新闻'
+          },{
+            title:'25岁研究生受伤后舍不得买贵支架，致血栓转移到肺部，逝世后捐献器官',
+            time:'2023-05-27',
+            picPath:'/pages/homepage/images/new16.png',
+            author:'九派新闻'
+          },{
+            title:'艰难开往乌克兰的F-16战机',
+            time:'2023-05-27',
+            picPath:'/pages/homepage/images/new17.png',
+            author:'中国新闻周刊'
+          },{
+            title:'安徽女子死后被摘取器官案：被害人儿子涉嫌敲诈涉案医生被立案',
+            time:'2023-05-27',
+            picPath:'/pages/homepage/images/new18.png',
+            author:'澎湃新闻'
+          },{ 
+            title:'红星深度｜23只小熊猫之死：警方解救后寄养动物园 涉案野生动物救助待规范',
+            time:'2023-05-15',
+            picPath:'/pages/homepage/images/new1.png',
+            author:'成都商报红星新闻'
+          },
+          {
+            title:'曾荣立一等战功的战区上将司令员发声，透露最新信号！',
+            time:'2023-05-15',
+            picPath:'/pages/homepage/images/new2.png',
+            author:'政知见'
+          },
+          {
+            title:'山西10岁男孩失联身亡，嫌疑人系生母与继父 知情人：生母曾称是孩子偷钱挨打后跑了',
+            time:'2023-05-24',
+            picPath:'/pages/homepage/images/new3.png',
+            author:'成都商报红星新闻'
+          },
+          {
+            title:'红星观察｜珠峰攀登生意经：有尊享版套餐，还有“被人抬着上山”一条龙服务',
+            time:'2023-05-23',
+            picPath:'/pages/homepage/images/new4.png',
+            author:'红星深度'
+          },/////////
+          {
+            title:'武汉一小学生校园内被老师驾车碾死 校方：没酒驾，孩子跑到车前蹲着',
+            time:'2023-05-24',
+            picPath:'/pages/homepage/images/new5.png',
+            author:'都市快报橙柿互动'
+          },{
+            title:'今年会有多少新生儿？产科一线：过去一天分娩七八个，现在几天一个',
+            time:'2023-05-24',
+            picPath:'/pages/homepage/images/new6.png',
+            author:'每日经济新闻'
+          },{
+            title:'TikTok遭美国一州全面封杀！总裁周受资：违宪，已起诉',
+            time:'2023-05-24',
+            picPath:'/pages/homepage/images/new7.png',
+            author:'每日经济新闻'
+          },{
+            title:'四川一教师输液时不幸身亡，涉事诊所称尽全力抢救，卫健局介入调查',
+            time:'2023-05-24',
+            picPath:'/pages/homepage/images/new8.png',
+            author:'九派新闻'
+          },{
+            title:'美国会众议长麦卡锡：我们向中国借钱，不是为了养美国这些闲人',
+            time:'2023-05-21',
+            picPath:'/pages/homepage/images/new9.png',
+            author:'观察者网'
+          },{ 
+            title:'塞尔维亚总统宣布27日辞去执政党主席职务，现场数十万人雨中集结',
+            time:'2023-05-27',
+            picPath:'/pages/homepage/images/new10.png',
+            author:'观察者网'
+          },
+          {
+            title:'美国研究首次系统性定义“长新冠”，12种“后遗症”症状列入',
+            time:'2023-05-27',
+            picPath:'/pages/homepage/images/new11.png',
+            author:'第一财经'
+          },
+          {
+            title:'人均GDP吊打北上深，这些四线小城普遍“家里有矿、富得流油”',
+            time:'2023-05-27',
+            picPath:'/pages/homepage/images/new12.png',
+            author:'国民经略'
+          },
+          {
+            title:'9岁男孩失踪13年后尸骨现身山林地缝，两名村妇被警方带走',
+            time:'2023-05-27',
+            picPath:'/pages/homepage/images/new13.png',
+            author:'极目新闻'
+          },{
+            title:'男子称25元挂专家号，医生打20分钟电话聊拆迁：最后两句话打发我',
+            time:'2023-05-27',
+            picPath:'/pages/homepage/images/new14.png',
+            author:'极目新闻'
+          },/////////////////
+          {
+            title:'河北一派出所内烟花爆炸致4死2失联，4个月前当地曾发文禁止经营烟花',
+            time:'2023-05-27',
+            picPath:'/pages/homepage/images/new15.png',
+            author:'上游新闻'
+          },{
+            title:'25岁研究生受伤后舍不得买贵支架，致血栓转移到肺部，逝世后捐献器官',
+            time:'2023-05-27',
+            picPath:'/pages/homepage/images/new16.png',
+            author:'九派新闻'
+          },{
+            title:'艰难开往乌克兰的F-16战机',
+            time:'2023-05-27',
+            picPath:'/pages/homepage/images/new17.png',
+            author:'中国新闻周刊'
+          },{
+            title:'安徽女子死后被摘取器官案：被害人儿子涉嫌敲诈涉案医生被立案',
+            time:'2023-05-27',
+            picPath:'/pages/homepage/images/new18.png',
+            author:'澎湃新闻'
+          },{ 
+            title:'红星深度｜23只小熊猫之死：警方解救后寄养动物园 涉案野生动物救助待规范',
+            time:'2023-05-15',
+            picPath:'/pages/homepage/images/new1.png',
+            author:'成都商报红星新闻'
+          },
+          {
+            title:'曾荣立一等战功的战区上将司令员发声，透露最新信号！',
+            time:'2023-05-15',
+            picPath:'/pages/homepage/images/new2.png',
+            author:'政知见'
+          },
+          {
+            title:'山西10岁男孩失联身亡，嫌疑人系生母与继父 知情人：生母曾称是孩子偷钱挨打后跑了',
+            time:'2023-05-24',
+            picPath:'/pages/homepage/images/new3.png',
+            author:'成都商报红星新闻'
+          },
+          {
+            title:'红星观察｜珠峰攀登生意经：有尊享版套餐，还有“被人抬着上山”一条龙服务',
+            time:'2023-05-23',
+            picPath:'/pages/homepage/images/new4.png',
+            author:'红星深度'
+          },{
+            title:'武汉一小学生校园内被老师驾车碾死 校方：没酒驾，孩子跑到车前蹲着',
+            time:'2023-05-24',
+            picPath:'/pages/homepage/images/new5.png',
+            author:'都市快报橙柿互动'
+          },{
+            title:'今年会有多少新生儿？产科一线：过去一天分娩七八个，现在几天一个',
+            time:'2023-05-24',
+            picPath:'/pages/homepage/images/new6.png',
+            author:'每日经济新闻'
+          },{//////////////////
+            title:'TikTok遭美国一州全面封杀！总裁周受资：违宪，已起诉',
+            time:'2023-05-24',
+            picPath:'/pages/homepage/images/new7.png',
+            author:'每日经济新闻'
+          },{
+            title:'四川一教师输液时不幸身亡，涉事诊所称尽全力抢救，卫健局介入调查',
+            time:'2023-05-24',
+            picPath:'/pages/homepage/images/new8.png',
+            author:'九派新闻'
+          },{
+            title:'美国会众议长麦卡锡：我们向中国借钱，不是为了养美国这些闲人',
+            time:'2023-05-21',
+            picPath:'/pages/homepage/images/new9.png',
+            author:'观察者网'
+          },{ 
+            title:'塞尔维亚总统宣布27日辞去执政党主席职务，现场数十万人雨中集结',
+            time:'2023-05-27',
+            picPath:'/pages/homepage/images/new10.png',
+            author:'观察者网'
+          },
+          {
+            title:'美国研究首次系统性定义“长新冠”，12种“后遗症”症状列入',
+            time:'2023-05-27',
+            picPath:'/pages/homepage/images/new11.png',
+            author:'第一财经'
+          },
+          {
+            title:'人均GDP吊打北上深，这些四线小城普遍“家里有矿、富得流油”',
+            time:'2023-05-27',
+            picPath:'/pages/homepage/images/new12.png',
+            author:'国民经略'
+          },
+          {
+            title:'9岁男孩失踪13年后尸骨现身山林地缝，两名村妇被警方带走',
+            time:'2023-05-27',
+            picPath:'/pages/homepage/images/new13.png',
+            author:'极目新闻'
+          },{
+            title:'男子称25元挂专家号，医生打20分钟电话聊拆迁：最后两句话打发我',
+            time:'2023-05-27',
+            picPath:'/pages/homepage/images/new14.png',
+            author:'极目新闻'
+          },{
+            title:'河北一派出所内烟花爆炸致4死2失联，4个月前当地曾发文禁止经营烟花',
+            time:'2023-05-27',
+            picPath:'/pages/homepage/images/new15.png',
+            author:'上游新闻'
+          },{
+            title:'25岁研究生受伤后舍不得买贵支架，致血栓转移到肺部，逝世后捐献器官',
+            time:'2023-05-27',
+            picPath:'/pages/homepage/images/new16.png',
+            author:'九派新闻'
+          },{/////////
+            title:'艰难开往乌克兰的F-16战机',
+            time:'2023-05-27',
+            picPath:'/pages/homepage/images/new17.png',
+            author:'中国新闻周刊'
+          },{
+            title:'安徽女子死后被摘取器官案：被害人儿子涉嫌敲诈涉案医生被立案',
+            time:'2023-05-27',
+            picPath:'/pages/homepage/images/new18.png',
+            author:'澎湃新闻'
+          },{
+            title:'安徽女子死后被摘取器官案：被害人儿子涉嫌敲诈涉案医生被立案',
+            time:'2023-05-27',
+            picPath:'/pages/homepage/images/new18.png',
+            author:'澎湃新闻'
+          },{ 
+            title:'红星深度｜23只小熊猫之死：警方解救后寄养动物园 涉案野生动物救助待规范',
+            time:'2023-05-15',
+            picPath:'/pages/homepage/images/new1.png',
+            author:'成都商报红星新闻'
+          },
+          {
+            title:'曾荣立一等战功的战区上将司令员发声，透露最新信号！',
+            time:'2023-05-15',
+            picPath:'/pages/homepage/images/new2.png',
+            author:'政知见'
+          },
+          {
+            title:'山西10岁男孩失联身亡，嫌疑人系生母与继父 知情人：生母曾称是孩子偷钱挨打后跑了',
+            time:'2023-05-24',
+            picPath:'/pages/homepage/images/new3.png',
+            author:'成都商报红星新闻'
+          },
+          {
+            title:'红星观察｜珠峰攀登生意经：有尊享版套餐，还有“被人抬着上山”一条龙服务',
+            time:'2023-05-23',
+            picPath:'/pages/homepage/images/new4.png',
+            author:'红星深度'
+          },{
+            title:'武汉一小学生校园内被老师驾车碾死 校方：没酒驾，孩子跑到车前蹲着',
+            time:'2023-05-24',
+            picPath:'/pages/homepage/images/new5.png',
+            author:'都市快报橙柿互动'
+          },{
+            title:'今年会有多少新生儿？产科一线：过去一天分娩七八个，现在几天一个',
+            time:'2023-05-24',
+            picPath:'/pages/homepage/images/new6.png',
+            author:'每日经济新闻'
+          },{
+            title:'美国会众议长麦卡锡：我们向中国借钱，不是为了养美国这些闲人',
+            time:'2023-05-21',
+            picPath:'/pages/homepage/images/new9.png',
+            author:'观察者网'
+          }
+    ],
+    pageNum:1,
+    pageSize:10,
+    totalPage:1,
+}
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+app.use(bodyParser.json())
+
+// 设置路由，处理触底加载功能
+app.get('/api/homepage/bottom', (req, res) => {
+  const { pageNum } = req.query;
+  const {bios}=req.query;
+  const pageSize = 10;
+  
+  // 将 pageNum 转换为数字类型
+  const pageNumber = parseInt(pageNum);
+  const bios_=parseInt(bios);
+  
+  // 计算开始和结束索引
+  const startIndex = (pageNumber * pageSize+bios_)%80;
+  const endIndex = startIndex + pageSize;
+  
+  const new_pageNumber=Math.floor(startIndex/pageSize)+1;
+  //调整结束地址
+  if(endIndex>80){
+    const endIndex1=80;
+    const startIndex2=0;
+    const endIndex2=endIndex%80;
+    const newsData=data.newsList.slice(startIndex, endIndex1);
+    const finalData=newsData.concat(data.newsList.slice(startIndex2, endIndex2));
+    res.json({news:finalData,pageNum:new_pageNumber, bios:bios});
+  }
+  else{
+    const newsData=data.newsList.slice(startIndex, endIndex);
+    res.json({news:newsData,pageNum:new_pageNumber});
+  }
+});
+
+// 设置路由， 处理加载界面功能
+app.get('/api/homepage/load', (req, res)=>{
+  const pageSize=10;
+  const min=1;
+  const max=data.newsList.length/pageSize;
+
+  //生成页码和页内偏移量
+  const pageNum=Math.floor(Math.random()*(max-min+1))+min;
+  const bios=Math.floor(Math.random()*9);
+  
+  //计算起始地址和结束地址
+  const startIndex=(pageNum-1)*pageSize+bios;
+  const endIndex=startIndex+pageSize;
+  if(endIndex>80){
+    const endIndex1=80;
+    const startIndex2=0;
+    const endIndex2=endIndex%80;
+    const newsData=data.newsList.slice(startIndex, endIndex1);
+    const finalData=newsData.concat(data.newsList.slice(startIndex2, endIndex2));
+    res.json({news:finalData,pageNum:new_pageNumber, bios:bios});
+  }
+  else{
+    const newsData=data.newsList.slice(startIndex, endIndex);
+    res.json({news:newsData,pageNum:pageNum, bios:bios});
+  }
+})
+
+//处理下拉刷新功能
+app.get('/api/homepage/pull', (req, res)=>{
+  const{pageNum}=req.query;
+  const{bios}=req.query;
+  const pageSize=10;
+
+  const pageNumber=parseInt(pageNum);
+  const bios_=parseInt(bios);
+
+  //将pageNum加一，然后计算起始地址
+  const startIndex=(pageNumber*pageSize+bios_)%80;
+  const endIndex=startIndex+pageSize;
+  const new_pageNumber=Math.floor(startIndex/pageSize)+1;
+  //调整结束地址
+  if(endIndex>80){
+    const endIndex1=80;
+    const startIndex2=0;
+    const endIndex2=endIndex%80;
+    const newsData=data.newsList.slice(startIndex, endIndex1);
+    const finalData=newsData.concat(data.newsList.slice(startIndex2, endIndex2));
+    res.json({news:finalData,pageNum:new_pageNumber, bios:bios});
+  }
+  else{
+    const newsData=data.newsList.slice(startIndex, endIndex);
+    res.json({news:newsData,pageNum:new_pageNumber});
+  }
+})
+
+
+// 启动服务器
+const port = 3000; // 设置服务器监听的端口号
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
--- pages/homepage/homepage.js
+++ pages/homepage/homepage.js
@@ -5,80 +5,18 @@ Page({
    * 页面的初始数据
    */
   data: {
-    newsList:[
-      { 
-        title:'红星深度｜23只小熊猫之死：警方解救后寄养动物园 涉案野生动物救助待规范',
-        time:'2023-05-15',
-        picPath:'/pages/homepage/images/new1.png',
-        author:'成都商报红星新闻'
-      },
-      {
-        title:'曾荣立一等战功的战区上将司令员发声，透露最新信号！',
-        time:'2023-05-15',
-        picPath:'/pages/homepage/images/new2.png',
-        author:'政知见'
-      },
-      {
-        title:'山西10岁男孩失联身亡，嫌疑人系生母与继父 知情人：生母曾称是孩子偷钱挨打后跑了',
-        time:'2023-05-24',
-        picPath:'/pages/homepage/images/new3.png',
-        author:'成都商报红星新闻'
-      },
-      {
-        title:'红星观察｜珠峰攀登生意经：有尊享版套餐，还有“被人抬着上山”一条龙服务',
-        time:'2023-05-23',
-        picPath:'/pages/homepage/images/new4.png',
-        author:'红星深度'
-      },{
-        title:'武汉一小学生校园内被老师驾车碾死 校方：没酒驾，孩子跑到车前蹲着',
-        time:'2023-05-24',
-        picPath:'/pages/homepage/images/new5.png',
-        author:'都市快报橙柿互动'
-      },{
-        title:'今年会有多少新生儿？产科一线：过去一天分娩七八个，现在几天一个',
-        time:'2023-05-24',
-        picPath:'/pages/homepage/images/new6.png',
-        author:'每日经济新闻'
-      },{
-        title:'TikTok遭美国一州全面封杀！总裁周受资：违宪，已起诉',
-        time:'2023-05-24',
-        picPath:'/pages/homepage/images/new7.png',
-        author:'每日经济新闻'
-      },{
-        title:'四川一教师输液时不幸身亡，涉事诊所称尽全力抢救，卫健局介入调查',
-        time:'2023-05-24',
-        picPath:'/pages/homepage/images/new8.png',
-        author:'九派新闻'
-      },{
-        title:'福建厦门一男医生猥亵女体检者被拘留：卫健委决定吊销其执业资格',
-        time:'2023-05-24',
-        picPath:'/pages/homepage/images/new2.png',
-        author:'成都商报红星新闻'
-      },{
-        title:'山西10岁男孩失联身亡，嫌疑人系生母与继父 知情人：生母曾称是孩子偷钱挨打后跑了',
-        time:'2023-05-24',
-        picPath:'/pages/homepage/images/new2.png',
-        author:'成都商报红星新闻'
-      },{
-        title:'山西10岁男孩失联身亡，嫌疑人系生母与继父 知情人：生母曾称是孩子偷钱挨打后跑了',
-        time:'2023-05-24',
-        picPath:'/pages/homepage/images/new2.png',
-        author:'成都商报红星新闻'
-      },{
-        title:'山西10岁男孩失联身亡，嫌疑人系生母与继父 知情人：生母曾称是孩子偷钱挨打后跑了',
-        time:'2023-05-24',
-        picPath:'/pages/homepage/images/new2.png',
-        author:'成都商报红星新闻'
-      }
-
-    ]
+    pageNum:1,
+    pageSize:10,
+    newsList:[],
+    bios:0,
   },
+  
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad(options) {
-
+  onLoad:function(options) {
+    this.loadNews();//调用loadNews函数加载新闻列表
   },
 
   /**
@@ -112,15 +50,73 @@ Page({
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh() {
-
+  // 本质是获取首页的数据
+  onPullDownRefresh(){
+    const {newsList, pageNum, bios}=this.data;
+    wx.request({
+      url: 'http://127.0.0.1:3000/api/homepage/pull',
+      data:{
+        pageNum,
+        bios
+      },
+      success: res => {
+        const newsData = res.data.news;
+        const pNum=res.data.pageNum;
+        setTimeout(()=>{
+          this.setData({
+            newsList:newsData,
+            pageNum:pNum,
+          });
+          wx.stopPullDownRefresh();
+        }, 1000);
+      },
+    });
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom() {
+    const { pageNum, bios } = this.data;
+    wx.showLoading({
+      title: '加载中...',
+    });
+    wx.request({
+      url: 'http://127.0.0.1:3000/api/homepage/bottom',
+      data: {
+        pageNum: pageNum,
+        bios:bios
+      },
+      success: res => {
+        const newsData = res.data.news;
+        const pNum=res.data.pageNum;
+        setTimeout(() => {
+          this.setData({
+            newsList: this.data.newsList.concat(newsData),
+            pageNum:pNum,
+          });
+          wx.hideLoading();
+        }, 1000);
+      },
+    });
+  },
+  
 
+  loadNews(){//监听界面加载初始新闻列表
+    const { newsList, pageNum, bios } = this.data;
+    wx.request({
+      url: 'http://127.0.0.1:3000/api/homepage/load',
+      success: res => {
+        const newsData = res.data.news;
+        const pNum=res.data.pageNum;
+        const b=res.data.bios;
+        this.setData({
+          newsList: newsData,
+          pageNum: pNum,
+          bios:b
+        });
+      },
+    });
   },
 
   /**
