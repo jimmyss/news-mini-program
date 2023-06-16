4d06cc673c0bc80ed33d43ae5a61d5d0c0c3991a
jimmyss <3240429693@qq.com>
2023-06-16 10:50:12 +0800

    首页-顶部导航栏-优化

--- backend/index.js
+++ backend/index.js
@@ -524,7 +524,7 @@ const mysql = require('mysql');
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'qewxs132',
+  password: 'password',
   database: 'test'
 });
 
